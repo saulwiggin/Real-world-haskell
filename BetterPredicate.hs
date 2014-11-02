f16ae21743f2a04c059df103c67351254c92501e
Saul Wiggin <saulwiggin@googlemail.com>
2014-11-02 12:56:24 +0000

    add file finder

---
+++ BetterPredicate.hs
@@ -0,0 +1,16 @@
+-- file: ch09/BetterPredicate.hs
+import Control.Monad (filterM)
+import System.Directory (Permissions(..), getModificationTime, getPermissions)
+import System.Time (ClockTime(..))
+import System.FilePath (takeExtension)
+import Control.Exception (bracket, handle)
+import System.IO (IOMode(..), hClose, hFileSize, openFile)
+
+-- the function we wrote earlier
+import RecursiveContents (getRecursiveContents)
+
+type Predicate =  FilePath      -- path to directory entry
+               -> Permissions   -- permissions
+               -> Maybe Integer -- file size (Nothing if not file)
+               -> ClockTime     -- last modified
+               -> Bool
